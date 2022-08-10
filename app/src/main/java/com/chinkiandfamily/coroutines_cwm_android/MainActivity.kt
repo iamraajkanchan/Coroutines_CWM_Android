79e10fcb6a8d9dae866903a9900caec8d1730b2b
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-08-10 14:45:46 +0530

    Using lifeCycleScope

--- app/src/main/java/com/chinkiandfamily/coroutines_cwm_android/MainActivity.kt
+++ app/src/main/java/com/chinkiandfamily/coroutines_cwm_android/MainActivity.kt
@@ -2,10 +2,11 @@ package com.chinkiandfamily.coroutines_cwm_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.chinkiandfamily.coroutines_cwm_android.databinding.ActivityMainBinding
-import kotlinx.coroutines.CompletableJob
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private val PROGRESS_MAX = 100
@@ -19,26 +20,77 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        binding?.btnStartJob?.setOnClickListener {
+            if (!::job.isInitialized) {
+                initJob()
+                binding?.progressBarJob?.startJobCancel(job)
+            }
+        }
     }
 
-    fun initJob() {
+    private fun initJob() {
+        updateJobCompleteText("")
         with(binding ?: return) {
             btnStartJob.text = "Start Job #1"
-            tvCompleteText.text = ""
             job = Job()
             job.invokeOnCompletion {
                 it?.message.let {
                     var msg = it
                     if (msg.isNullOrBlank()) {
-                        msg = "Unknown Cancellation Error"
+                        msg = "Unknown Cancellation Error."
                     }
                     println("$job was cancelled $msg")
+                    showToast(msg)
                 }
+            }
+            progressBarJob.max = PROGRESS_MAX
+            progressBarJob.progress = PROGRESS_START
+        }
+    }
+
+    /**
+     * Extension Function for ProgressBar
+     * */
+    private fun ProgressBar.startJobCancel(job: Job) {
+        if (this.progress > 0) {
+            println("$job is already active. Cancelling...")
+            resetJob()
+        } else {
+            binding?.btnStartJob?.text = "Cancel Job #1"
+            lifecycleScope.launch(Dispatchers.IO + job) {
+                println("Coroutine $this is activated with job: $job")
+                for (i in PROGRESS_START..PROGRESS_MAX) {
+                    delay((JOB_TIME / PROGRESS_MAX).toLong())
+                    this@startJobCancel.progress = i
+                }
+                updateJobCompleteText("Job is complete")
             }
         }
     }
 
-    fun showToast(text: String) {
-        Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
+    private fun resetJob() {
+        if (job.isActive || job.isCompleted) {
+            job.cancel(CancellationException("Resetting Job"))
+        }
+        initJob()
+    }
+
+    private fun updateJobCompleteText(text: String) {
+        lifecycleScope.launch(Dispatchers.Main) {
+            binding?.tvCompleteText?.text = text
+        }
+    }
+
+    private fun showToast(text: String) {
+        lifecycleScope.launch {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
